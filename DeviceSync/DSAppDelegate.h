378970e1f5af47c78fe39be64585db7f65118f2d
yep <yep171@yahoo.com>
2013-11-11 21:26:59 +0100

    Initial Commit

---
+++ DeviceSync/DSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DSAppDelegate.h
+//  DeviceSync
+//
+//  Created by - on 11.11.13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DeviceSync/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DeviceSync
+//
+//  Created by - on 11.11.13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DSAppDelegate class]));
+    }
+}
